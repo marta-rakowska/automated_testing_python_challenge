38ec911d0432b51b38bfd0490a2c16acaa88fcb2
marta-rakowska <marta.rakowska91@gmail.com>
2023-04-17 19:11:57 +0200

    refactor: dashboard selectors

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -2,26 +2,18 @@ import time
 
 from pages.base_page import BasePage
 
-# poprawić selektory!!!
 class Dashboard(BasePage):
     main_page_xpath = "//*[text()='Main page']"
     players_xpath = "//*[text()='Players']"
     polski_xpath = "//*[text()='Polski']"
     sign_out_xpath = "//*[text()='Sign out']"
-    players_count_xpath = "//*[@id='__next']/div[1]/main/div[2]/div[1]/div/div[1]"
-    matches_count_xpath = "//*[@id='__next']/div[1]/main/div[2]/div[2]/div/div[1]"
-    reports_count_xpath = "//*[@id='__next']/div[1]/main/div[2]/div[3]/div/div[1]"
-    events_count_xpath = "//*[@id='__next']/div[1]/main/div[2]/div[4]/div/div[1]"
-    dev_team_contact_xpath = "//*[@id='__next']/div[1]/main/div[3]/div[1]/div/div[3]/a/span[1]"
-    add_player_xpath = "//*[@id='__next']/div[1]/main/div[3]/div[2]/div/div/a/button/span[1]"
-    logo_scouts_panel_xpath = "//*[@id='__next']/div[1]/main/div[3]/div[1]/div/div[1]"
-    shortcuts_xpath = "//*[@id='__next']/div[1]/main/div[3]/div[2]/div/div/h2"
-    activity_xpath = "//*[@id='__next']/div[1]/main/div[3]/div[2]/div/div/h2"
-    last_created_player_xpath = "//*[@id='__next']/div[1]/main/div[3]/div[3]/div/div/h6[1]"
-    last_updated_player_xpath = "//*[@id='__next']/div[1]/main/div[3]/div[3]/div/div/h6[2]"
-    last_created_match_xpath = "//*[@id='__next']/div[1]/main/div[3]/div[3]/div/div/h6[3]"
-    last_updated_match_xpath = "//*[@id='__next']/div[1]/main/div[3]/div[3]/div/div/h6[4]"
-    last_updated_report_xpath = "//*[@id='__next']/div[1]/main/div[3]/div[3]/div/div/h6[5]"
+    dev_team_contact_xpath = "//*[text()='Dev team contact']"
+    add_player_xpath = "//*[text()='Add player']"
+    last_created_player_xpath = "//*[@id='__next']/div[1]/main/div[3]/div[3]/div/div/a[1]/button/span[1]"
+    last_updated_player_xpath = "//*[@id='__next']/div[1]/main/div[3]/div[3]/div/div/a[2]/button/span[1]"
+    last_created_match_xpath = "//*[@id='__next']/div[1]/main/div[3]/div[3]/div/div/a[3]/button/span[1]"
+    last_updated_match_xpath = "//*[@id='__next']/div[1]/main/div[3]/div[3]/div/div/a[4]/button/span[1]"
+    last_updated_report_xpath = "//*[@id='__next']/div[1]/main/div[3]/div[3]/div/div/a[5]/button/span[1]"
 
     expected_title = "Scouts panel"
     dashboard_url = "https://scouts-test.futbolkolektyw.pl/"
